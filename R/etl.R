90297eafc13ec5a73274838d27affb324605af0e
rplain1 <ryan.plain1@gmail.com>
2025-04-21 21:06:13 -0600

    ugh

--- R/etl.R
+++ R/etl.R
@@ -7,27 +7,26 @@
 #' @param dbname Character. DB name, defaults to my dog `belle`
 #' @param force_rebuild Logical. If `TRUE`, forces a rebuild of the database. Defaults to `FALSE`.
 #' @param tblname name or DBI::Id(schema, table) of the table location
-#' @param db_connection A `DBI::dbConnect()` object
+#' @param dpath Path for custom db
 #'
 #' @return No return value. Updates the nflfastR database in place.
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' update_w()  # Updates the database in the default location
-#' update_nflfastR_db(dbdir = "data/nfl", force_rebuild = TRUE)  # Forces a rebuild
+#' update_db()  # Updates the database in the default location
 #' }
 update_db <- function(
   dbdir = '~/.db',
   dbname = 'belle',
   force_rebuild = FALSE,
-  tblname = DBI::Id(schema = "BASE", table = 'wehoop_wnba_pbp'),
-  db_connection = DBI::dbConnect(duckdb::duckdb(), Sys.getenv("DB_PATH"))
+  dbpath = Sys.getenv("DB_PATH_WNBA")
 ) {
+  db_connection <- DBI::dbConnect(duckdb::duckdb(), dbpath)
+  on.exit(DBI::dbDisconnect(db_connection), add = TRUE)
   wehoop::update_wnba_db(
     dbdir = dbdir,
     dbname = dbname,
-    tblname = tblname,
     force_rebuild = force_rebuild,
     db_connection = db_connection
   )
